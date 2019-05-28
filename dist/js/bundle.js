a56ba3b44ae804428dda503f9107a26972567087
NikitaV <varfol007@gmail.com>
2019-05-28 21:37:42 +0300

    create calc

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -112,9 +112,71 @@ function calc() {
     let size = document.getElementById('size'),
         material = document.getElementById('material'),
         options = document.getElementById('options'),
-        price = document.querySelector('.calc-price');
+        price = document.querySelector('.calc-price'),  
+        promocode = document.querySelector('.promocode'),
+        total = 0;
 
-    
+    size.addEventListener('change', function() {
+        let  sizePrice = +size.options[size.selectedIndex].value,
+            koef = +material.options[material.selectedIndex].value,
+            optPrice = +options.options[options.selectedIndex].value;
+
+        if (size.options[size.selectedIndex].value != "0" && material.options[material.selectedIndex].value != "0") {
+            total = sizePrice * koef + optPrice;
+            if (promocode.value == "IWANTPOPART") {
+                price.innerHTML = total / 30;
+            } else {
+                price.innerHTML = total;    
+            }
+        } else {
+             price.innerHTML = 'Для расчета нужно выбрать размер картины и материал картины';
+        }
+    });
+
+    material.addEventListener('change', () => {
+        let  sizePrice = +size.options[size.selectedIndex].value,
+            koef = +material.options[material.selectedIndex].value,
+            optPrice = +options.options[options.selectedIndex].value;
+
+        if (size.options[size.selectedIndex].value != "0" && material.options[material.selectedIndex].value != "0") {
+            total = sizePrice * koef + optPrice;
+            if (promocode.value == "IWANTPOPART") {
+                price.innerHTML = total / 100 * 70;
+            } else {
+                price.innerHTML = total;    
+            }
+        } else {
+            price.innerHTML = 'Для расчета нужно выбрать размер картины и материал картины';
+        }
+    });
+    options.addEventListener('change', () => {
+        let  sizePrice = +size.options[size.selectedIndex].value,
+            koef = +material.options[material.selectedIndex].value,
+            optPrice = +options.options[options.selectedIndex].value;
+
+        if (size.options[size.selectedIndex].value != "0" && material.options[material.selectedIndex].value != "0") {
+            total = sizePrice * koef + optPrice;
+            if (promocode.value == "IWANTPOPART") {
+                price.innerHTML = total / 100 * 70;
+            } else {
+                price.innerHTML = total;    
+            }
+        } else {
+            price.innerHTML = 'Для расчета нужно выбрать размер картины и материал картины';
+        }
+    });
+    promocode.addEventListener('input', () => {
+        if (size.options[size.selectedIndex].value != "0" && material.options[material.selectedIndex].value != "0") {
+            total = sizePrice * koef + optPrice;
+            if (promocode.value == "IWANTPOPART") {
+                price.innerHTML = total / 100 * 70;
+            } else {
+                price.innerHTML = total;    
+            }
+        } else {
+            price.innerHTML = 'Для расчета нужно выбрать размер картины и материал картины';
+        }
+    });
 }
 
 module.exports = calc;
